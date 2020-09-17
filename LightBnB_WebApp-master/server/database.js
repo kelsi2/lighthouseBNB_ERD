5a75a2bdc5a3810e2bb1933d101a3d26b04d2fad
Kelsi Proulx <kelsiproulx1919@gmail.com>
2020-09-17 19:01:09 +0000

    add getAllReservations function

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -76,7 +76,19 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  return pool.query(`
+  SELECT reservations.*, properties.*, AVG(property_reviews.rating) AS average_rating
+  FROM reservations
+    JOIN properties ON reservations.property_id = properties.id
+    JOIN property_reviews ON reservations.property_id = properties.id
+  WHERE reservations.end_date > now()
+  ::date AND reservations.guest_id = 1
+  GROUP BY reservations.id, properties.id
+  ORDER BY start_date
+  LIMIT 10;
+  `)
+    .then(res => res.rows)
+    .catch(err => console.log(err));
 };
 exports.getAllReservations = getAllReservations;
 
